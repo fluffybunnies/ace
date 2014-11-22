e46f5d375048fc78da30c5207764daf8ba53e00d
alec <alec.hulce@gmail.com>
2014-11-21 22:56:52 -0800

    u

--- web/ace/controllers/Demo.php
+++ web/ace/controllers/Demo.php
@@ -9,8 +9,8 @@ use \ace\ControllerAbstract;
 
 class Demo extends ControllerAbstract {
 
-	private $defaultTo = array('volcomstoner2689@gmail.com');
-	private $defaultFrom = 'acquiremint-notifs@gmail.com';
+	private $defaultEmailTo = array('volcomstoner2689@gmail.com');
+	private $defaultEmailFrom = 'acquiremint-notifs@gmail.com';
 
 
 	public function emailCsvWithNode(){
@@ -18,7 +18,22 @@ class Demo extends ControllerAbstract {
 	}
 
 	public function emailCsvWithPhp(){
-		return $this->getSampleData();
+		$params = $this->getInput(array(
+			'email_to' => false,
+		));
+		if (!$params['email_to'])
+			$params['email_to'] = $this->$defaultEmailTo;
+
+		$fileName = WEBROOT.'/../out/demo-csvwithphp.'.time().'.csv';
+		$data = $this->getSampleData();
+		$this->generateCsvFromArray($fileName, $data);
+		if (!is_file($res))
+			throw new \Exception('output file not found');
+
+		return array(
+			'outputFileName' => basename($fileName),
+			'emailedTo' => $params['email_to'],
+		);
 	}
 
 
@@ -26,6 +41,24 @@ class Demo extends ControllerAbstract {
 		return include WEBROOT.'/../sample-data.php';
 	}
 
+	private function generateCsvFromArray($writeToPath, $data){
+		if (!isset($data[0]))
+			throw new Exception('Invalid input');
+		if (!touch($writeToPath))
+			throw new Exception('Error making file');
+		if (!($f = fopen($writeToPath, 'w')))
+			throw new Exception('Error opening file for writing');
+		
+		$headers = array();
+		foreach ($data[0] as $k => $v)
+			$headers[] = $k;
+		fputcsv($f, $headers);
 
+		foreach ($data as $fields)
+			fputcsv($f, $fields);
+
+		if (!empty($f))
+			fclose($f);
+	}
 
 }
