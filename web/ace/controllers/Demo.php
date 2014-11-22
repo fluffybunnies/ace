eb80ddbaafd02636194fac3e958658593c3978dd
alec <alec.hulce@gmail.com>
2014-11-21 23:09:49 -0800

    u

--- web/ace/controllers/Demo.php
+++ web/ace/controllers/Demo.php
@@ -24,7 +24,7 @@ class Demo extends ControllerAbstract {
 		if (!$params['email_to'])
 			$params['email_to'] = $this->defaultEmailTo;
 
-		$fileName = REPOROOT.'/out/demo-csvwithphp.'.time().'.csv';
+		$fileName = WEBROOT.'/../out/demo-csvwithphp.'.time().'.csv';
 		$data = $this->getSampleData();
 		$this->generateCsvFromArray($fileName, $data);
 		if (!is_file($res))
