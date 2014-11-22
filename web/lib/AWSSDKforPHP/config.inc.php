ea02418f2bcd27997280ae13f53c2bc5cb374343
alec <alec.hulce@gmail.com>
2014-11-22 00:23:45 -0800

    u

--- web/lib/AWSSDKforPHP/config.inc.php
+++ web/lib/AWSSDKforPHP/config.inc.php
@@ -57,11 +57,11 @@ CFCredentials::set(array(
 
 		// Amazon Web Services Key. Found in the AWS Security Credentials. You can also pass
 		// this value as the first parameter to a service constructor.
-		'key' => Ace::config('sesKey'),
+		'key' => Ace::getConfig('sesKey'),
 
 		// Amazon Web Services Secret Key. Found in the AWS Security Credentials. You can also
 		// pass this value as the second parameter to a service constructor.
-		'secret' => Ace::config('sesSecret'),
+		'secret' => Ace::getConfig('sesSecret'),
 
 		// This option allows you to configure a preferred storage type to use for caching by
 		// default. This can be changed later using the set_cache_config() method.
