066b30033254fc3ff3cfb39cdc5a4187c4edc448
alec <alec.hulce@gmail.com>
2014-11-22 22:52:51 -0800

    u

--- web/ace/autoload.php
+++ web/ace/autoload.php
@@ -3,6 +3,8 @@
 spl_autoload_register(function($name) {
 	if (strpos($name,'\\') === false)
 		return;
+	if (strpos($name,'\\') === 0)
+		$name = '\\'.$name;
 	$path = str_replace('\\','/',$name);
 	$class = basename($path);
 	$dir = \WEBROOT.'/'.dirname($path);
