06f26b7edc3812357157e7e287e0881831c2b584
alec <alec.hulce@gmail.com>
2014-11-23 00:03:03 -0800

    u

--- bin/demo-emailCsv.js
+++ bin/demo-emailCsv.js
@@ -3,11 +3,15 @@
 */
 
 var fs = require('fs')
-,csvPars = require('csv-parse')
-,ut= require('../ut')
+,csvOut = require('../csvout')
+//,ut = require('../ut')
 ,inData = require(__dirname+'/../sample-data.json')
 ,outFile = __dirname+'/web/public-out/demo-csvwithnode.'+ut.fileTime()+'.csv'
 ;
 
 console.log(['inData: '+inData, 'outFile: '+outFile].join('\n'),'\n');
 
+createCsv(inData, function(err,data){
+	if (err)
+		return console.log('Err: ',err);
+});
