099c49f2019c2c5ae5661cf166765a013148bfba
alec <alec.hulce@gmail.com>
2014-11-22 20:59:58 -0800

    u

--- web/ace/autoload.php
+++ web/ace/autoload.php
@@ -1,16 +1,14 @@
 <?php
 
 spl_autoload_register(function($name) {
-	//static $dirCache = array();
+	if (strpos($name,'\\') === false)
+		return;
+	$path = str_replace('\\','/',$name);
+	$class = basename($path);
+	$dir = \WEBROOT.'/'.dirname($path);
 
-	if (strpos($name,'\\') !== false) {
-		$path = str_replace('\\','/',$name);
-		$class = basename($path);
-		$dir = \WEBROOT.'/'.dirname($path);
-
-		if (is_file($dir.'/'.$class.'.php')) {
-			require_once $dir.'/'.$class.'.php';
-			return;
-		}
+	if (is_file($dir.'/'.$class.'.php')) {
+		require_once $dir.'/'.$class.'.php';
+		return;
 	}
 });
