dfe75b594c3cc31a24eb69b09a5d8cf0f44bdc4f
alec <alec.hulce@gmail.com>
2014-11-22 23:46:40 -0800

    u

--- bin/demo-emailCsv.js
+++ bin/demo-emailCsv.js
@@ -5,6 +5,6 @@
 var ut= require('./ut')
 ;
 
-var fileNAme = __dirname+'/web/public-out/demo-csvwithphp.'+ut.fileTime()+'.csv';
+var fileName = __dirname+'/web/public-out/demo-csvwithphp.'+ut.fileTime()+'.csv';
 console.log(fileName);
 //generateCsv(
