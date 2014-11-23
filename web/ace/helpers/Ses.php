ea055a2ef4dd3e8539f6c61e954d49be3bfc2620
alec <alec.hulce@gmail.com>
2014-11-22 19:55:27 -0800

    u

--- web/ace/helpers/Ses.php
+++ web/ace/helpers/Ses.php
@@ -141,7 +141,7 @@ HTML;
 		$msg .= "Subject: $subject\n";
 		$msg .= "MIME-Version: 1.0\n";
 		$msg .= "Content-Type: multipart/alternative;";
-		$msg .= "Content-Type: Multipart/Mixed;";
+		//$msg .= "Content-Type: Multipart/Mixed;";
 		$msg .= " boundary=\"$b\"\n\n";
 		$msg .= "--$b\n";
 		$msg .= "Content-Type: text/plain; charset=utf-8\n";
