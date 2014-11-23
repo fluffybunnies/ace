fe6cbbbb5c85d0a5918083020d80ba47e812781e
alec <alec.hulce@gmail.com>
2014-11-22 23:09:40 -0800

    u

--- web/ace/helpers/Ses.php
+++ web/ace/helpers/Ses.php
@@ -139,7 +139,7 @@ class Ses extends HelperAbstract {
 
 		$msg .= "--\n";
 
-		if (!empty($_GET['debug'])) {echo `git rev-parse HEAD`;echo $msg . "\n";}
+		//if (!empty($_GET['debug'])) {echo `git rev-parse HEAD`;echo $msg . "\n";}
 		return base64_encode($msg);
 	}
 
