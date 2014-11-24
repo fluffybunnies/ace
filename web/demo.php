55e2d7f2ed0e62e062d963932cbef16c6b7101cc
alec <alec.hulce@gmail.com>
2014-11-23 22:11:26 -0800

    u

--- web/demo.php
+++ web/demo.php
@@ -16,9 +16,9 @@ use \ace\Ace;
 $(function(){
 	$('.ace-smile').each(function(){
 		var $el = $(this);
-		$.getJSON('/ace/api/demo/smile',function(data){
+		$.getJSON('/ace/api/demo/smilea',function(data){
 			if (!(data && data.data))
-				return $el.html('api error' + data.error?': '+data.error:'');
+				return $el.html('api error' + (data.error?': '+data.error:''));
 			$el.html(data.data);
 		});
 	});
