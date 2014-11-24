d44d6de8e2679a80ede5f9617809688918a15c68
alec <alec.hulce@gmail.com>
2014-11-24 03:23:53 -0800

    u

--- web/bootshell.php
+++ web/bootshell.php
@@ -13,6 +13,7 @@ define('WEBROOT', dirname(__FILE__));
 define('APP_PATH', WEBROOT.'/ace');
 include APP_PATH.'/autoload.php';
 use \ace\Ace;
+use \ace\Router;
 define('REQUEST_PATH', rtrim( Ace::g($_SERVER, array('DOCUMENT_URI','REDIRECT_URL'), ''), '/' ));
 
 Ace::loadConfig( WEBROOT.'/config.php', WEBROOT.'/config.local.php' );
@@ -26,6 +27,6 @@ foreach (array('RDS_HOSTNAME','RDS_PORT','RDS_USERNAME','RDS_PASSWORD','RDS_DB_N
 }
 */
 
-use \ace\Router;
+
 Router::route(REQUEST_PATH);
 
