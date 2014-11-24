ccee1469acaf9e6d5a9f8f196c1acf0be27a35c3
alec <alec.hulce@gmail.com>
2014-11-24 00:13:55 -0800

    u

--- web/ace/controllers/Demo.php
+++ web/ace/controllers/Demo.php
@@ -37,9 +37,7 @@ class Demo extends ControllerAbstract {
 		$webroot = WEBROOT;
 		$cmd = "/usr/local/bin/node $webroot/../bin/demo-emailCsv.js --emailTo=$emailTo --emailFrom=$emailFrom $subjectParam";
 		if (!empty($_GET['debug'])) echo "$cmd\n<br />";
-		//return `$cmd`;
-		`$cmd`;
-		sleep(1);
+		return `$cmd &`;
 	}
 
 	public function emailCsvWithPhp(){
