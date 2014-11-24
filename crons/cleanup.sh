f255f0173a9dffc39ed70d022d4df72b230a197f
alec <alec.hulce@gmail.com>
2014-11-23 21:07:52 -0800

    u

--- crons/cleanup.sh
+++ crons/cleanup.sh
@@ -4,10 +4,10 @@ installDir=$1
 echo "installDir: $installDir"
 
 echo ""
-echo "Clean files in web/public-out/ 7+ days old"
+echo "Clean files in web/public-out/ 1+ days old"
 # print0 and -0 are for spaces in filenames
-find $installDir/web/public-out -mtime +7 -type f -print0 | xargs -0 rm
+find $installDir/web/public-out -mtime +1 -type f -print0 | xargs -0 rm
 
 echo ""
-echo "Clean files in out/ 7+ days old"
-find $installDir/out -mtime +7 -type f -print0 | xargs -0 rm
+echo "Clean files in out/ 1+ days old"
+find $installDir/out -mtime +1 -type f -print0 | xargs -0 rm
