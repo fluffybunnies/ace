e86634363ac158cecaa399afd493e46fa606e75a
alec <alec.hulce@gmail.com>
2014-11-24 04:57:50 -0800

    u

--- web/ace/helpers/Ses.php
+++ web/ace/helpers/Ses.php
@@ -120,7 +120,7 @@ class Ses extends HelperAbstract {
 		if (!empty($opts['bcc']))
 			$msg .= 'Bcc: '.(is_array($opts['bcc']) ? implode(',',$opts['bcc']) : $opts['bcc'])."\n";
 		if (!empty($opts['reply_to']))
-			$msg .= 'Reply: '.$opts['reply_to']."\n";
+			$msg .= 'Reply-to: '.$opts['reply_to']."\n";
 		$msg .= "Subject: $subject\n";
 		$msg .= "MIME-Version: 1.0\n";
 		//$msg .= "Content-Type: multipart/alternative;";
