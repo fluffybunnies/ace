37bd216c70f4bda65d4f8631516799f1e326c5d1
alec <alec.hulce@gmail.com>
2014-12-03 22:14:51 -0800

    :-)

--- web/ace/misc/wordpress.php
+++ web/ace/misc/wordpress.php
@@ -2,7 +2,7 @@
 
 namespace ace\misc;
 
-user \ace\Ace;
+use \ace\helpers\Tracking;
 
 remove_filter( 'the_content', 'wpautop' );
 remove_filter( 'the_excerpt', 'wpautop' );
@@ -14,16 +14,5 @@ function hook_wphead(){
 }
 
 function hook_wpfooter(){
-	if (!($googleAnalyticsId = Ace::getConfig('googleAnalyticsId')))
-		return;
-	?>
-	<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	ga('create', '<?php echo $googleAnalyticsId; ?>', 'auto');
-	ga('send', 'pageview');
-	</script>
-	<?php
+	Tracking::pageView();
 }
