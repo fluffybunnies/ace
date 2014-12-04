26963ce6880b0190347c4ebc7dc521c2c0d1ae59
alec <alec.hulce@gmail.com>
2014-12-03 22:15:50 -0800

    :-)

--- web/ace/misc/wordpress.php
+++ web/ace/misc/wordpress.php
@@ -4,8 +4,10 @@ namespace ace\misc;
 
 use \ace\helpers\Tracking;
 
+// disable this: "Changes double line-breaks in the text into HTML paragraphs (<p>...</p>)."
 remove_filter('the_content', 'wpautop');
 remove_filter('the_excerpt', 'wpautop');
+
 add_action('wp_head', '\ace\misc\hook_wphead');
 add_action('wp_footer', '\ace\misc\hook_wpfooter');
 
