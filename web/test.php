21f7b447a00bc84b507843a4b78505c41304697a
alec <alec.hulce@gmail.com>
2014-12-07 22:11:04 -0800

    :-)

--- web/test.php
+++ web/test.php
@@ -26,7 +26,7 @@ function doIt($str){
 function sp($v){
 	$v = html_entity_decode($v);
 	$v = preg_replace_callback('/(&#([0-9]+);)/',function($m){
-		echo $m[1]."\n<br />";
+		v($m);
 		return chr($m[1]);
 	},$v);
 }
