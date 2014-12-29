a386a2950df6aac2bf391ab6827263bb8dbcd163
alec <alec.hulce@gmail.com>
2014-12-28 21:11:06 -0800

    :-)

--- crons/cleanup.sh
+++ crons/cleanup.sh
@@ -6,8 +6,12 @@ echo "installDir: $installDir"
 echo ""
 echo "Clean files in web/public-out/ 7+ days old"
 # print0 and -0 are for spaces in filenames
-find $installDir/web/public-out -mtime +7 -type f -print0 | xargs -0 rm
+find $installDir/web/public-out -mtime +7 -type f -print0 | xargs -0 rm -v
 
 echo ""
 echo "Clean files in out/ 1+ days old"
-find $installDir/out -mtime +1 -type f -print0 | xargs -0 rm
+find $installDir/out -mtime +1 -type f -print0 | xargs -0 rm -v
+
+# show count + remove...
+# remove: find ./out  -type f -print0 | tee >(xargs -0 echo | wc -w) | xargs -0 rm
+# issue: the count is appended and piped to rm
