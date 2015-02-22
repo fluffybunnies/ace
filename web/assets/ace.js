5853b32c92b171ca536848fceeafbbef339cdea5
alec <alec.hulce@gmail.com>
2015-02-22 02:10:26 -0800

    | (• ◡•)|

--- web/assets/ace.js
+++ web/assets/ace.js
@@ -97,7 +97,7 @@ AceBase.prototype._getEvt = function(key){
 }
 AceBase.prototype._fireSubs = function(key){
 	var evt = this._getEvt(key), subs = evt.subs.slice(0), i = 0;
-	console.log('erg', key, evt);
+	console.log('erg', key, evt, subs);
 	for (;i<subs.length;++i) {
 		subs[i].cb(evt.error,evt.data);
 	}
