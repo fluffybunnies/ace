4e1fbc684a625970090dbd0bdd2f794f72cf4acf
alec <alec.hulce@gmail.com>
2015-04-09 22:29:12 -0700

    ¬_¬

--- web/assets/ace.ui.twitter.js
+++ web/assets/ace.ui.twitter.js
@@ -90,6 +90,7 @@ ace.ui.register('twitter',{
 			,match = {}
 		;
 		$.each(tweet.entities.urls,function(i,item){
+			console.log(item.url, i);
 			if (match[item.url])
 				return true;
 			match[item.url] = true;
